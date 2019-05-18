6c856784164882f666208f25062c0c73ec061a39
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-18 16:01:32 +0900

    Remove console.log

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,5 +1,4 @@
 export default function ({ store, redirect }) {
-  console.log('store.getters.authenticated', store.getters.authenticated)
   // If the user is not authenticated
   if (!store.getters.authenticated) {
     return redirect('/login')
