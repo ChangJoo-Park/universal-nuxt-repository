fecda0f34cbe6dd94a8f4dc484bbe3047ad39d46
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-19 12:49:42 +0900

    eslint 수정

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -7,21 +7,43 @@
       <table class="w-full border border-2 text-center">
         <thead class="w-full border border-b-2 bg-gray-300">
           <tr>
-            <th class="py-2 font-mono">ID</th>
-            <th class="py-2 font-mono">username</th>
-            <th class="py-2 font-mono">email</th>
-            <th class="py-2 font-mono">createdAt</th>
-            <th class="py-2 font-mono">updatedAt</th>
-            <th class="py-2 font-mono">actions</th>
+            <th class="py-2 font-mono">
+              ID
+            </th>
+            <th class="py-2 font-mono">
+              username
+            </th>
+            <th class="py-2 font-mono">
+              email
+            </th>
+            <th class="py-2 font-mono">
+              createdAt
+            </th>
+            <th class="py-2 font-mono">
+              updatedAt
+            </th>
+            <th class="py-2 font-mono">
+              actions
+            </th>
           </tr>
         </thead>
         <tbody class="w-full">
           <tr v-for="(row, index) in users" :key="index">
-            <td class="py-2 border-b-2">{{ row.id }}</td>
-            <td class="py-2 border-b-2">{{ row.username }}</td>
-            <td class="py-2 border-b-2">{{ row.email }}</td>
-            <td class="py-2 border-b-2">{{ row.createdAt }}</td>
-            <td class="py-2 border-b-2">{{ row.updatedAt }}</td>
+            <td class="py-2 border-b-2">
+              {{ row.id }}
+            </td>
+            <td class="py-2 border-b-2">
+              {{ row.username }}
+            </td>
+            <td class="py-2 border-b-2">
+              {{ row.email }}
+            </td>
+            <td class="py-2 border-b-2">
+              {{ row.createdAt }}
+            </td>
+            <td class="py-2 border-b-2">
+              {{ row.updatedAt }}
+            </td>
             <td class="py-2 border-b-2">
               <button @click="edit">
                 EDIT
