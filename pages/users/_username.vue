722c7f240fb5609805e1712bc3920578dd121032
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-21 22:15:26 +0900

    사용자 이름 기준 페이지 추가

--- pages/users/_username.vue
+++ pages/users/_username.vue
@@ -1,14 +1,55 @@
 <template>
-  <div class="container mx-auto">
-    <h1>
-      Hello World
-      {{ user }}
-    </h1>
+  <div class="container mx-auto flex">
+    <div class="flex-none" style="min-width: 280px;">
+      <h1>
+        {{ user.username }}
+        {{ user.email }}
+      </h1>
+    </div>
+    <div>
+      <div>
+        <ul>
+          <li class="inline-block">
+            <nuxt-link :to="{ name: 'users-username', params: { username: user.username} }">
+              Overview
+            </nuxt-link>
+          </li>
+          <li class="inline-block">
+            <nuxt-link :to="{ name: 'users-username-series', params: { username: user.username} }">
+              Series
+            </nuxt-link>
+          </li>
+          <li class="inline-block">
+            <nuxt-link :to="{ name: 'users-username-posts', params: { username: user.username} }">
+              Posts
+            </nuxt-link>
+          </li>
+          <template v-if="sameUser">
+            <li class="inline-block">
+              <nuxt-link :to="{ name: 'users-username-setting', params: { username: user.username} }">
+                Setting
+              </nuxt-link>
+            </li>
+          </template>
+        </ul>
+      </div>
+      <nuxt-child />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
+  computed: {
+    ...mapGetters({
+      authenticated: 'authenticated',
+      loggedInUser: 'user'
+    }),
+    sameUser() {
+      return this.authenticated && this.loggedInUser.id === this.user.id
+    }
+  },
   asyncData({ app: { $repositories }, params, error }) {
     console.log('params -> username => ', params.username)
     return $repositories.user.show(params.username)
